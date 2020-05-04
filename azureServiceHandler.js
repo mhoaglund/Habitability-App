45aad7ef2fdd7a56ab04750726c36a5b01a93dd5
mhoaglund <maxwell.hoaglund@gmail.com>
2020-05-03 20:34:47 -0500

    stub for service handler, added packages

---
+++ azureServiceHandler.js
@@ -0,0 +1,13 @@
+var azure = require('azure-storage');
+
+//TODO: private getSecret for auth
+//TODO: public upload for adding to blob
+//TODO: public delete for removing from blob
+
+exports.upload = function () {
+    return false;
+};
+
+exports.delete = function () {
+    return false;
+};
